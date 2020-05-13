edab3a0fa2c005edd53de6e1ecdee273e60aedf8
Andrew <pemberton5150@gmail.com>
2020-05-12 21:05:31 -0600

    added some client rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,61 @@
 import React, { Component } from "react";
+import axios from 'axios'
 
 
 class App extends Component {
-  // constructor
+
+  state = {
+    searchTerm: '',
+    books: []
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target;
+
+    this.setState({ [name]: value});
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.searchTerm)
+      .then((result)=>{
+        console.log(result);
+        
+        this.setState({books: result.data.items})
+      })
+  }
+
   render() {
     return (
-      <div>
-        <div className="containter">
+      
+        <div className="containter" onSubmit={this.handleSubmit}>
           <div className="jumbotron shadow"></div>
+          <form id="search-form" name="searchForm">
+            <section>
+              <input 
+                type="text" 
+                name="searchTerm" 
+                placeholder="some texty text" 
+                value={this.state.searchTerm}
+                onChange={this.handleChange}
+                />
+            </section>
+            <section>
+              <button>Search</button>
+            </section>
+            </form>
+            <div>
+              {this.state.books.map((book)=>(
+                <div>
+                  {book.volumeInfo.title}
+                </div>
+              ))}
+            </div>
         </div>
-      </div>
+      
     );
   }
 }
