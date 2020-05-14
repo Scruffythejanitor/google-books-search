7eda2e57df20420e413d6fa8837979a2610f6c9a
Scruffythejanitor <58636685+Scruffythejanitor@users.noreply.github.com>
2020-05-14 15:52:26 -0600

    added seach and saved sections

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from 'axios';
+// import { Route } from "react-router";
 
 
 class App extends Component {
@@ -10,9 +11,9 @@ class App extends Component {
   }
 
   handleChange = (event) => {
-    const {name, value} = event.target;
+    const { name, value } = event.target;
 
-    this.setState({ [name]: value});
+    this.setState({ [name]: value });
 
   }
 
@@ -21,41 +22,74 @@ class App extends Component {
 
     axios
       .get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.searchTerm)
-      .then((result)=>{
+      .then((result) => {
         console.log(result);
-        
-        this.setState({books: result.data.items})
+
+        this.setState({ books: result.data.items })
       })
   }
 
   render() {
     return (
-      
-        <div className="containter" onSubmit={this.handleSubmit}>
-          <div className="jumbotron shadow"></div>
-          <form id="search-form" name="searchForm">
-            <section>
-              <input 
-                type="text" 
-                name="searchTerm" 
-                placeholder="some texty text" 
-                value={this.state.searchTerm}
-                onChange={this.handleChange}
-                />
-            </section>
-            <section>
-              <button>Search</button>
-            </section>
-            </form>
-            <div>
-              {this.state.books.map((book)=>(
-                <div>
-                  {book.volumeInfo.title}
+
+      <div className="containter">
+        <div className="jumbotron jumbotron-fluid shadow text-center" onSubmit={this.handleSubmit}>
+          <button
+            className="btn btn-lg btn-outline-secondary mx-5"
+            type="button"
+            data-toggle="collapse"
+            data-target="#searchCollapse"
+            aria-expanded="false"
+            aria-controls="searchCollapse"
+          >
+            Search
+          </button>
+          <button className="btn btn-lg btn-outline-secondary m-5">
+            Saved
+          </button>
+          <div className="container collapse" id="searchCollapse">
+            <form id="search-form" name="searchForm">
+              <section>
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    name="searchTerm"
+                    className="form-control"
+                    placeholder="Search For Book Here"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    value={this.state.searchTerm}
+                    onChange={this.handleChange}
+                  />
+                  <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" type="submit">Search</button>
+                  </div>
                 </div>
-              ))}
-            </div>
+              </section>
+            </form>
+          </div>
         </div>
-      
+        <div className="card-columns m-4">
+          {this.state.books.map((book) => (
+
+            <div className="card shadow">
+              <img className="card-img-top " src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+              <div className="card-body">
+                <h5 className="card-title">{book.volumeInfo.title}</h5>
+                <p className="card-text">{book.volumeInfo.description}</p>
+              </div>
+              <div class="card-footer">
+                {/* <small class="text-muted">Last updated 3 mins ago</small> */}
+                <form method="POST" action=".routes/api-routes/save">
+                  <button type="submit" className="btn btn-success">Save Book</button>
+                </form>
+              </div>
+            </div>
+
+          ))}
+        </div>
+      </div>
+
     );
   }
 }
